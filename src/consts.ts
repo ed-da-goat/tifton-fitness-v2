47fd0473932adb912aa47552323cd8eecfb3acbb
ed-da-goat <EDWARD.AHRENS.3@GMAIL.COM>
2026-04-26 13:50:54 -0400

    refactor: centralize site constants and add 404 page
    - Extract addresses, phone, URLs, and GloFox branch ID into src/consts.ts
    - Update all pages and components to import from consts
    - Add 404.astro page
    - Add .env.example for documentation
    - Remove SPA catch-all redirect from netlify.toml

---
+++ src/consts.ts
@@ -0,0 +1,22 @@
+export const PHONE_DISPLAY = "(229) 340-6228";
+export const PHONE_TEL = "tel:+12293406228";
+export const PHONE_E164 = "+12293406228";
+
+export const ADDRESS_STREET = "130 Hunt Rd B7";
+export const ADDRESS_CITY = "Tifton";
+export const ADDRESS_STATE = "GA";
+export const ADDRESS_ZIP = "31793";
+export const ADDRESS_FULL = `${ADDRESS_STREET}, ${ADDRESS_CITY}, ${ADDRESS_STATE} ${ADDRESS_ZIP}`;
+export const MAPS_DIRECTIONS_URL =
+  "https://www.google.com/maps/dir/?api=1&destination=130+Hunt+Rd+B7,+Tifton,+GA+31793";
+export const MAPS_EMBED_URL =
+  "https://maps.google.com/maps?q=130+Hunt+Rd+B7%2C+Tifton%2C+GA+31793&z=16&output=embed";
+
+export const SITE_NAME = "Tifton Fitness";
+export const SITE_URL = "https://tiftonfitness.com";
+export const GA_MEASUREMENT_ID = "G-QZPJV6J4Y8";
+export const GLOFOX_BRANCH_ID = "69321543c5a54a285f0fff2c";
+export const FORMSPREE_FORM_ID = "xvzdykdz";
+
+export const GEO_LAT = 31.462;
+export const GEO_LNG = -83.523;
